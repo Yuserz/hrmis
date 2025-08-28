d11c1ffed6eb5a7810c56ae2aa673b620c158ae5
ianDrilon03 <ian@findfoundation.org.au>
2025-08-28 14:53:19 +0800

    Adding attendance sheet

--- helpers/navItems.ts
+++ helpers/navItems.ts
@@ -1,5 +1,11 @@
 import { SideMenu } from '@/lib/types/menus'
-import { LayoutDashboard, Users, TowerControl, Plane } from 'lucide-react'
+import {
+  LayoutDashboard,
+  Users,
+  TowerControl,
+  Plane,
+  FileText
+} from 'lucide-react'
 
 export const adminMenus = (id: string): SideMenu[] => {
   return [
@@ -7,6 +13,12 @@ export const adminMenus = (id: string): SideMenu[] => {
       title: 'Dashboard',
       url: `/backend/${id}/dashboard`,
       icon: LayoutDashboard,
+      isActive: true
+    },
+    {
+      title: 'Attendance',
+      url: `/backend/${id}/attendance`,
+      icon: FileText,
       isActive: true
     },
     {
